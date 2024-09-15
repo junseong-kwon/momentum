2d62ac18d5acb072967abee9635a34fefa9dc1da
권준성 <rnjswnstjd13@gmail.com>
2024-09-15 14:58:10 +0900

    js 공부 1

--- app.js
+++ app.js
@@ -1,43 +1,14 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function hangleTitleClick() {
-  console.log("mouse click!");
-}
-
-function handleMouseEnter() {
-  h1.innerText = "Mouse is here!";
-}
-
-function hangleMouseLeave() {
-  h1.innerText = "Mouse is gone!";
-}
-
-function hangleWindowResize() {
-  document.body.style.backgroundColor = "tomato";
-}
-
-function hangleWindowCopy() {
-  alert("copier!");
-}
-
-function hangleWindowPaste() {
-  alert("paste!!!!");
-}
-
-function hangleWindowOffline() {
-  alert("SoS no WIFI");
-}
-
-function hangleWindowOnline() {
-  alert("Good WIFI");
+  const currentColor = h1.style.color;
+  let newColor;
+  if (currentColor === "blue") {
+    newColor = "tomato";
+  } else {
+    newColor = "blue";
+  }
+   h1.style.color = newColor;
 }
 
 h1.addEventListener("click", hangleTitleClick);
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", hangleMouseLeave);
-
-window.addEventListener("resize", hangleWindowResize);
-window.addEventListener("copy", hangleWindowCopy);
-window.addEventListener("paste", hangleWindowPaste);
-window.addEventListener("offline", hangleWindowOffline);
-window.addEventListener("online", hangleWindowOnline);
